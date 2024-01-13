fee018588062c1e1aa664f78480864440d6c282e
RAraghavarora <raghavarora999@yahoo.in>
2024-01-13 22:58:18 +0530

    Revert "Add conda.el to manual dir"
    This reverts commit c95fd9072395294c39410658501574e274c4277c.

--- init.el
+++ init.el
@@ -129,8 +129,7 @@
     :global-prefix "C-SPC")
   (rune/leader-keys
    "t" '(:ignore t :which-key "toggles")
-   "tt" '(counsel-load-theme :which-key "choose theme")
-   "tm" '(set-mark-command :which-key "set mark"))
+   "tt" '(counsel-load-theme :which-key "choose theme"))
 )
 
 (use-package evil
@@ -190,12 +189,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(conda-anaconda-home "C:/Users/raghav/miniconda3")
  '(package-selected-packages
-   '(lsp-pyright visual-fill-column conda forge elpy chess jupyter ssh-deploy org-bullets all-the-icons telega magit auctex ef-themes which-key rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-modeline counsel-projectile command-log-mode))
- '(safe-local-variable-values
-   '((python-shell-virtualenv-root . "C:/Users/raghav/miniconda3/envs/default")
-     (conda-project-env-path . "default"))))
+   '(ssh-deploy org-bullets all-the-icons telega magit auctex ef-themes which-key rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-modeline counsel-projectile command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -262,26 +257,4 @@
 
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
-
-(setq tramp-verbose 10)
-
-(use-package conda
-  :config (progn
-            (conda-env-initialize-interactive-shells)
-            (conda-env-initialize-eshell)
-            (conda-env-autoactivate-mode t)
-            (setq conda-env-home-directory (expand-file-name "C:/Users/raghav/miniconda3"))
-            ))
-
-(setq tab-always-indent 'complete)
-(setq completion-styles '(flex basic partial-completion emacs22))
-(put 'scroll-left 'disabled nil)
-
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
-
-(set-language-environment "UTF-8")
 
