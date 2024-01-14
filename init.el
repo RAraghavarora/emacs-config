4be66dc4e999ee669b8c789dadc0b4ce0c41a61b
RAraghavarora <raghavarora999@yahoo.in>
2024-01-14 18:21:24 +0530

    Trying to remove evil parantheses error. No luck

--- init.el
+++ init.el
@@ -133,6 +133,11 @@
    "tm" '(set-mark-command :which-key "set mark"))
 )
 
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 (use-package evil
   :init
   (setq evil-want-integration t)
@@ -144,17 +149,12 @@
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
+  ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal)
-)
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
 (use-package hydra)
 
