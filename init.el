1db0fef2d7ce6a26d19c414d7b7bac330bfc5194
RAraghavarora <raghavarora999@yahoo.in>
2024-01-14 13:04:09 +0530

    Add corfu for autocomplete

--- init.el
+++ init.el
@@ -192,7 +192,7 @@
  ;; If there is more than one, they won't work right.
  '(conda-anaconda-home "C:/Users/raghav/miniconda3")
  '(package-selected-packages
-   '(lsp-pyright visual-fill-column conda forge elpy chess jupyter ssh-deploy org-bullets all-the-icons telega magit auctex ef-themes which-key rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-modeline counsel-projectile command-log-mode))
+   '(corfu lsp-pyright visual-fill-column conda forge elpy chess jupyter ssh-deploy org-bullets all-the-icons telega magit auctex ef-themes which-key rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-modeline counsel-projectile command-log-mode))
  '(safe-local-variable-values
    '((python-shell-virtualenv-root . "C:/Users/raghav/miniconda3/envs/default")
      (conda-project-env-path . "default"))))
@@ -284,4 +284,43 @@
                           (lsp))))  ; or lsp-deferred
 
 (set-language-environment "UTF-8")
+
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  :hook ((prog-mode . corfu-mode)
+         (shell-mode . corfu-mode)
+         (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+
+;; A few more useful configurations...
+;; (use-package emacs
+;;   :init
+;;   ;; TAB cycle if there are only few candidates
+;;   (setq completion-cycle-threshold 3)
+
+;;   ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+;;   ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+;;   (setq read-extended-command-predicate
+;;         #'command-completion-default-include-p)
+
+;;   ;; Enable indentation+completion using the TAB key.
+;;   ;; `completion-at-point' is often bound to M-TAB.
+;;   (setq tab-always-indent 'complete))
 
